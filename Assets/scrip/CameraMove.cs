15f5b0a762e24cea5485c463fa9893b197c6b85e
Artori4z <kongphop.buch@bumail.net>
2024-10-01 22:27:41 +0700

    game

---
+++ Assets/scrip/CameraMove.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    private int currentCameraIndex = 0;
+    [SerializeField]public Vector3 vector;
+    [SerializeField]private float RotationSpeed;
+    private float xRotation;
+    [SerializeField] private float xMin;
+    [SerializeField] private float xMax;
+    [SerializeField] private float yMin;
+    [SerializeField] private float yMax;
+    private float yRotation;
+    private void Update()
+    {
+        float UpDown = Input.GetKey(KeyCode.W) ? 1 : Input.GetKey(KeyCode.S) ? -1 : 0;
+        float LeftRight = Input.GetKey(KeyCode.A) ? 1 : Input.GetKey(KeyCode.D) ? -1 : 0;
+        xRotation -= UpDown * RotationSpeed * Time.deltaTime;
+        yRotation -= LeftRight * RotationSpeed * Time.deltaTime;
+        xRotation = Mathf.Clamp(xRotation,yMin,yMax);
+        yRotation = Mathf.Clamp(yRotation,xMin,xMax);
+        transform.localRotation = Quaternion.Euler(xRotation,yRotation,0f);
+    }
+
+}
